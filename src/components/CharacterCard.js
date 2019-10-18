3f4a36519bd79ff7072bcdc10547c3ce19551691
Kerri-Ann <kaybee1jam@yahoo.com>
2019-10-18 12:18:56 -0400

    added some styles

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,10 +1,20 @@
 import React from "react";
+import styled from 'styled-components';
 
+const Card = styled.div`
+    width: 30%;
+    background: grey;
+    color: #fff;
+    padding: 20px;
+    border-radius: 5px;
+    margin-top: 10px;
+    margin-bottom: 10px;
+`
 export default function CharacterCard(props) {
   const { character } = props;
 
   return (
-    <div className="card">
+    <Card>
       <h2>{character.name}</h2>
       <div className="image">
         <img src={character.image} />
@@ -14,6 +24,6 @@ export default function CharacterCard(props) {
         <p>Gender: {character.gender}</p>
         <p>Status: {character.status}</p>
       </div>
-    </div>
+    </Card>
   )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,23 @@
 import React from "react";
-import { Link } from 'react-router-dom';
-import CharacterList from "./CharacterList";
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Nav = styled.nav`
+  text-align: center;
+`
+const StyledLink = styled(NavLink)`
+  text-decoration: none;
+  padding: 0 10px;
+`
 
 export default function Header() {
   return (
     <header className="ui centered">
       <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
-      <nav>
-        <Link to='/'>Home</Link>
-        <Link to='/characters'>Characters</Link>
-      </nav>
+      <Nav>
+        <StyledLink  to='/'>Home</StyledLink>
+        <StyledLink to='/characters'>Characters</StyledLink>
+      </Nav>
     </header>
   );
 }
