d5eca34f18abaf39233997e1f905e9f0cd0173bf
Kerri-Ann <kaybee1jam@yahoo.com>
2019-10-18 13:01:27 -0400

    made search form functional

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,25 +1,46 @@
 import React, { useEffect, useState } from "react";
 import Axios from "axios";
 import CharacterCard from "./CharacterCard";
+import SearchForm from "./SearchForm";
 
 export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
+
   const [charactersData, setCharactersData] = useState([]);
+  const [filteredChracters, setFilteredCharacters] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
+
+  // Get value from search input.
+  function searchCharacters(e) {
+    setSearchValue(e.target.value);
+  }
 
   useEffect(() => {
     Axios.get('https://rickandmortyapi.com/api/character/')
     .then(response => {
-      console.log(response.data.results);
+
       setCharactersData(response.data.results);
+      setFilteredCharacters(response.data.results);
     })
     .catch(error => {
       console.log('Error: Unable to fetch data', error);
     })
   }, []);
 
+  useEffect(() => {
+    setFilteredCharacters(
+      charactersData.filter((character) => {
+        return character.name.toLowerCase().includes(searchValue.toLowerCase());
+      })
+    )
+
+  }, [searchValue]);
+
+  console.log(searchValue);
+  console.log(filteredChracters);
   return (
     <section className="character-list">
-      {charactersData.map(character => (
+      <SearchForm searchCharacters={searchCharacters} />
+      {filteredChracters.map(character => (
         <CharacterCard character={character} key={character.id}/>
       ))}
     </section>
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,31 @@
-import React, { useState } from "react";
+import React from "react";
+import styled from 'styled-components';
 
-export default function SearchForm() {
- 
+const SearchSection = styled.div`
+  text-align: center;
+  margin: 20px auto;
+`
+
+const SearchInput = styled.input`
+  padding: 5px 20px;
+  font-size: 18px;
+  border-radius: 5px;
+  border: 2px solid slateblue;
+`
+
+export default function SearchForm(props) {
+  const { searchCharacters } = props;
   return (
-    <section className="search-form">
-     // Add a search form here
-    </section>
+    <SearchSection className="search-form">
+     <form>
+        <SearchInput 
+          type="text" 
+          name="search" 
+          id="name" 
+          placeholder="Search character" 
+          onChange={searchCharacters} 
+        />
+     </form>
+    </SearchSection>
   );
 }
